4e7ac4d311ed8f7e497f43b134cef8dab06d0a92
Charles K.S <ctmbr@protonmail.com>
2022-07-16 13:31:33 -0700

    Work on Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Nav() {
-  return <div>Doodoodoo</div>;
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <a href="about">About</a>
+        </li>
+        <li>
+          <a href="contact">Contact</a>
+        </li>
+        <li>
+          <a href="portfolio">Portfolio</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
