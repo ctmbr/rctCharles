8313867e73262933dd303bf1d0f47d1ce47065d8
Charles K.S <ctmbr@protonmail.com>
2022-07-20 10:05:09 -0700

    Added content to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./styles/about.css";
+import "./styles/btn.css";
 
 export default function AboutMe() {
-  return <div>Dadada</div>;
+  return (
+    <section className="hero">
+      <div className="first">
+        <img src="./img/Charles.jpg" className="img" alt="Charles Portrait" />
+      </div>
+      <div className="content">
+        <span className="title">Freelance Web Developer</span>
+        <h1>
+          Hello, I’m <span>Charles</span>
+        </h1>
+        <p>A North America-based developer. This is my Portfolio.</p>
+      </div>
+    </section>
+  );
 }
